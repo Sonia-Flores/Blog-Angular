7d75cb448ceec05ccd47d262a4c6c82035ecca22
Sonia-GE <sflores@grupoepelsa.com>
2023-07-10 17:37:48 +0200

    Interface - array

--- src/app/components/blog/blog.component.ts
+++ src/app/components/blog/blog.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { New } from 'src/app/interfaces/inew.interface';
 
 @Component({
   selector: 'app-blog',
@@ -6,5 +7,26 @@ import { Component } from '@angular/core';
   styleUrls: ['./blog.component.css']
 })
 export class BlogComponent {
+ 
+  arrNews : New[] = [];
+
+  constructor() {
+    this.arrNews = [
+      {
+          title: "Ángel se olvida de su pelo verde",
+          img_url: "https://via.placeholder.com/600/92c952",
+          body_new: "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Pariatur mollitia amet nam necessitatibus dolorem voluptatem quia illo dolores, unde excepturi id enim aut molestias, cumque, voluptatibus quo minus omnis? Ratione alias, molestiae provident amet facilis quia, debitis adipisci cupiditate, beatae nam temporibus sit? Perspiciatis amet, laboriosam voluptatum necessitatibus sit fugiat.",
+          date: new Date
+      },
+      {
+        title: "Ángel se olvida de su pelo verde",
+        img_url: "https://via.placeholder.com/600/92c952",
+        body_new: "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Pariatur mollitia amet nam necessitatibus dolorem voluptatem quia illo dolores, unde excepturi id enim aut molestias, cumque, voluptatibus quo minus omnis? Ratione alias, molestiae provident amet facilis quia, debitis adipisci cupiditate, beatae nam temporibus sit? Perspiciatis amet, laboriosam voluptatum necessitatibus sit fugiat.",
+        date: new Date
+      }
+    
+    ]
+  }
+
 
 }
---
+++ src/app/interfaces/inew.interface.ts
@@ -0,0 +1,6 @@
+export interface New {
+    title: string;
+    img_url: string;
+    body_new: string;
+    date: Date;
+}
